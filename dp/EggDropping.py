48cafb6b50ced65142bd9e636cea16d63bcec491
Sahil Ahmad <sahmad@EX2INDIA.LOCAL>
2025-08-18 12:44:40 +0530

    optimise egg drop puzzle

--- dp/EggDropping.py
+++ dp/EggDropping.py
@@ -15,3 +15,42 @@ class Solution :
             dict[(e,f)] = ans
             return ans
         return solve(n,k)
+    
+    def eggDropOptimised(self, n, k):
+        memo = {}
+
+        def solve(e: int, f: int):
+            if f <= 1 or e == 1:
+                return f
+
+            if (e, f) in memo:
+                return memo[(e, f)]
+
+            ans = float('inf')
+            low, high = 1, f
+
+            while low <= high:
+                mid = (low + high) // 2
+
+                if (e-1, mid-1) in memo:
+                    broken = memo[(e-1, mid-1)]
+                else:
+                    memo[(e-1, mid-1)] = broken = solve(e-1, mid-1)
+
+                if (e, f-mid) in memo:
+                    not_broken = memo[(e, f-mid)]
+                else:
+                    memo[(e, f-mid)] = not_broken = solve(e, f-mid)
+
+                temp = 1 + max(broken, not_broken)
+                ans = min(ans, temp)
+
+                if broken > not_broken:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+            memo[(e, f)] = ans
+            return ans
+
+        return solve(n, k)
