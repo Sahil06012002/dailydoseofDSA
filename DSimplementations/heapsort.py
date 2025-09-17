8feccff8515a44616a6cb445820956af60e8e80a
Sahil Ahmad <sahil.ahmedfbd@gmail.com>
2025-09-17 15:08:15 +0530

    implement heap sort

--- DSimplementations/heapsort.py
+++ DSimplementations/heapsort.py
@@ -92,30 +92,44 @@ mh = MinHeap([])
 mh.insert(45)
 mh.insert(4)
 mh.insert(42)
+        
 
+class HeapSort :
 
-
-
+    def conver_to_maxheap(arr) : 
+        n = len(arr)
+        for i in range(n //2 -1,-1,-1) :
+            HeapSort.heapify(arr, i , n)
     
 
 
+    def heapify(arr, i , n) :
+        largest = i
+        leftIndex= (2 * i) + 1
+        rightIndex = (2 * i ) + 2
 
+        if leftIndex < n and arr[largest] <  arr[leftIndex] :
+            largest = leftIndex
+
+        if rightIndex < n and arr[largest] < arr[rightIndex] :
+            largest = rightIndex
+
+        if largest != i:
+            temp = arr[i]
+            arr[i] = arr[largest]
+            arr[largest] = temp
+            HeapSort.heapify(arr, largest,n)
+
+
+    def heap_sort(arr) :
+        n = len(arr)
+        HeapSort.conver_to_maxheap(arr)
+
+        for i in range(n-1, 0 , -1) :
+            arr[0] , arr[i] = arr[i] , arr[0]
+            HeapSort.heapify(arr, 0,i)
 
 
 
 
         
-
-
-
-
-        
-
-#     def sortC(self,nums : List[int]) :
-#         HeapSort.minHeap(nums)
-#         # print(nums)
-
-# asol = HeapSort()
-# asol.sortC("sahil")
-        
-
