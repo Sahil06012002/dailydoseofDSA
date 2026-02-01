134288905fa850eca6b360f676230ea6e2927ebd
Sahil06012002 <sahil.ahmedfbd@gmail.com>
2026-02-01 20:13:46 +0530

    implemment optimised leetcode 3010

--- dailychalenge/leetcode3010.py
+++ dailychalenge/leetcode3010.py
@@ -35,3 +35,35 @@ class Solution:
         
 
         return f_element + s_element + t_element
+    
+
+    def two_min(nums) :
+        f_smallest = nums[0]
+        f_index = 0
+        s_smallest = nums[0]
+
+        for i in range(1,len(nums)) :
+            if nums[i] < f_smallest :
+                f_smallest = nums[i]
+                f_index = i
+
+            if nums[i] < s_smallest and f_index != i :
+                s_smallest = nums[i]
+        return f_smallest, s_smallest 
+
+
+# Ideal Pattern Approach 
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        f_element = nums[0]
+        f_smallest = float('inf')
+        s_smallest = float('inf')
+
+        for i in range(1,len(nums)) :
+            if nums[i] <= f_smallest :
+                s_smallest = f_smallest
+                f_smallest = nums[i]
+            else :
+                s_smallest = min(s_smallest, nums[i])
+        
+        return f_element + f_smallest + s_smallest
