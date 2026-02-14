d0f8260da23ecf1dcd346466c1f7e887ed11a01a
Sahil06012002 <sahil.ahmedfbd@gmail.com>
2026-02-15 01:02:17 +0530

    implement bottom up for subset sum

--- practice/dp/SubsetSum.java
+++ practice/dp/SubsetSum.java
@@ -8,13 +8,37 @@ public class SubsetSum {
         if (n == 0)
             return false;
 
-        boolean take = false;
         if (arr[n - 1] <= target) {
-            take = subset(arr, target - arr[n - 1], n - 1);
+            return subset(arr, target - arr[n - 1], n - 1) || subset(arr, target, n - 1);
+        } else {
+            return subset(arr, target, n - 1);
         }
-        boolean leave = subset(arr, target, n - 1);
-        return take || leave;
+    }
 
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        int n = arr.length;
+
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (i == 0)
+                    dp[i][j] = false;
+                if (j == 0)
+                    dp[i][j] = true;
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
     }
 
     public static void main(String[] args) {
